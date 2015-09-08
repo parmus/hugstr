385790f6c83e99d6e67d4cd907f5eb9f2b115f0c
Martin Parm <parmus@spotify.com>
2015-09-08 02:35:12 +0200

    Updated sensortest implementation

--- src/sensortest/sensortest.ino
+++ src/sensortest/sensortest.ino
@@ -9,12 +9,16 @@ const unsigned int numLEDs = 16;
 
 // Touch sensor
 const int TouchSensorPin = 1;
-const unsigned int TouchSensitivity = 75;  // Higher number -> more sensitive
+const unsigned int TouchSensitivity = 100;  // Higher number -> more sensitive
 
 //==========================================
 
 
 Adafruit_NeoPixel LEDs = Adafruit_NeoPixel(numLEDs, LEDPin, NEO_GRB + NEO_KHZ800);
+
+const uint32_t RED = Adafruit_NeoPixel::Color(255, 0, 0);
+const uint32_t GREEN = Adafruit_NeoPixel::Color(0, 255, 0);
+const unsigned long maxValue = TouchSensitivity * 2;
 
 unsigned long lastTick;
 
@@ -29,7 +33,12 @@ void loop() {
   unsigned long tick = millis();
   
   if (tick - lastTick > 10){
-    setBar(analogRead(TouchSensorPin));
+    unsigned long value = analogRead(TouchSensorPin);
+    if (value > TouchSensitivity){
+      setBar(value, maxValue, RED);
+    } else {
+      setBar(value, maxValue, GREEN);
+    }
     lastTick = tick;
   }
 }
@@ -38,18 +47,20 @@ unsigned int LEDIndex(unsigned int index){
   return (index + 2) % 16;
 }
 
-void setBar(unsigned long value) {  // Should be 0-1000
+uint32_t scaleColor(uint32_t color, uint8_t brightness){
+  return scaleColor((uint8_t)(color >> 16), (uint8_t)(color >>  8),(uint8_t) color, brightness);
+}
+
+uint32_t scaleColor(uint8_t r, uint8_t g, uint8_t b, uint8_t brightness){
+  return Adafruit_NeoPixel::Color((r * brightness) >> 8, (g * brightness) >> 8, (b * brightness) >> 8);
+}
+
+void setBar(unsigned long value, unsigned long maxValue, uint32_t color) {
   unsigned long steps = LEDs.numPixels() * 255;
-  unsigned int c;
   
-  long stepper = value * steps / (TouchSensitivity*2);
+  long stepper = (maxValue-min(maxValue, value)) * steps / maxValue;
   for(unsigned int i=0; i < LEDs.numPixels(); i++){
-    c = min(stepper, 255);
-    if (value > TouchSensitivity){
-      LEDs.setPixelColor(LEDIndex(i), LEDs.Color(c, 0, 0));
-    } else {
-      LEDs.setPixelColor(LEDIndex(i), LEDs.Color(0, c, 0));
-    }
+    LEDs.setPixelColor(LEDIndex(i), scaleColor(color, uint8_t(min(stepper, 255))));
     
     stepper = ( stepper>255 ? stepper - 255 : 0);
   }
